23ae470772c5b179f82b6e8329af467a264db35d
lazycoder1 <gautamgsabhahit@gmail.com>
2018-07-05 18:08:33 +0530

    course code lesson 2 complete

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -1,32 +1,42 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+// const web3 = new Web3(ganache.provider());
 
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
+const { interface, bytecode } = require('../compile');
 
-// class Car {
-// 	park() {
-// 		return 'stopped';
-// 	}
+let accounts;
+let inbox;
 
-// 	drive() {
-// 		return 'vroom';
-// 	}
-// }
+beforeEach(async () => {
+	//Get a list of all accounts 
+	accounts = await web3.eth.getAccounts();
 
-// let car;
+	// Use one of those accounts to deploy the contract
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments: ['Hi There!'] })
+		.send({ from: accounts[0], gas: '1000000' });
 
-// beforeEach(() => {
-// 	car = new Car();
-// });
+	inbox.setProvider(provider);
+});
 
-// describe('Car', () => {
-// 	it('can park', () => {
-// 		assert.equal(car.park(), 'stopped'); 
-// 	});
+describe('Inbox',() => {
+	it('deploys a contract',() => {
+		assert.ok(inbox.options.address)
+	});
 
-// 	it('can drive', () => {
-// 		assert.equal(car.drive(), 'vroom'); 
-// 	});
-// });
+	it('has a default message',async () => {
+		const message = await inbox.methods.message().call();
+		assert.equal(message,'Hi There!');
+	});
+
+	it('can change the message',async () => {
+		await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+		const message = await inbox.methods.message().call();
+		assert.equal(message,'bye')
+	});
+});
+
